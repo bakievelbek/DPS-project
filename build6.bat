2fc086a68f20a5a5176ea713ef2f495edd736801
Ian Murnane <ian.murnane001@stud.fh-dortmund.de>
2023-02-19 23:30:49 +0100

    updated certs, added 5 and 6

--- build6.bat
+++ build6.bat
@@ -4,7 +4,7 @@
 @REM cmake -DCMAKE_PREFIX_PATH="C:\code\aws-iot-device-sdk-cpp-v2-build" -DCMAKE_BUILD_TYPE="Release" .
 @REM cmake --build . --config "Release"
 
-.\Release\DPS-project.exe --cert .\certs\PlatoonDevice2\certificate.pem.crt --client_id PlatoonDevice2 --endpoint avb6i8gas5stv-ats.iot.us-east-1.amazonaws.com --key .\certs\PlatoonDevice2\private.pem.key --topic platoon/channel
+.\Release\DPS-project.exe --cert .\certs\PlatoonDevice6\certificate.pem.crt --client_id PlatoonDevice6 --endpoint avb6i8gas5stv-ats.iot.us-east-1.amazonaws.com --key .\certs\PlatoonDevice6\private.pem.key --topic platoon/channel
 
 if %ERRORLEVEL% neq 0 (
     echo Program exited with an error level of %ERRORLEVEL%.
